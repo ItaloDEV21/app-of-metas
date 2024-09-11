42050480004b011e3ce8d1ba894d6d3272414333
ItaloDEV21 <sampaioitalo21@gmail.com>
2024-09-11 11:39:31 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts') //Aqui vira um objeto, e de dentro desse objeto quero apenas o "select"
+const { select, input, checkbox } = require('@inquirer/prompts') //Aqui vira um objeto, e de dentro desse objeto quero apenas o "select"
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -20,6 +20,33 @@ const cadastrarMeta = async () => {
     metas.push({ 
         value: meta, checked: false
     })
+}
+
+const listarMeta = async () => {
+    const respostas = await checkbox({
+        message: "use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finlizar essa etapa",
+        choices: [...metas],
+        instructions: false
+    })
+
+    if(respostas.length == 0){
+        console.log("nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s) ')
 }
 
 const start = async () => {
@@ -50,7 +77,7 @@ const start = async () => {
             console.log(metas)
             break
         case "listar":
-            console.log("vamos listar ")
+            await listarMeta()
             break
         case "sair":
             console.log(" até a próxima ")
