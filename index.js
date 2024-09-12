4dbd13b4fb09375f75ed4a1d7380f950008d97c3
ItaloDEV21 <sampaioitalo21@gmail.com>
2024-09-12 11:22:59 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -49,6 +49,22 @@ const listarMeta = async () => {
     console.log('Meta(s) marcadas como concluída(s) ')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0 ){
+        console.log('Não existem metas realizadas! :(')
+        return 
+    }
+
+    await select ({
+        message: "Metas realizadas ",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
     
   while(true) {
@@ -65,6 +81,10 @@ const start = async () => {
                 value: "listar"
             },
             {
+                name: "Metas realizadas",
+                value: "realizadas"
+            },
+            {
                 name: "Sair", 
                 value: "sair"
             }
@@ -78,6 +98,9 @@ const start = async () => {
             break
         case "listar":
             await listarMeta()
+            break
+        case "realizadas":
+            await metasRealizadas()
             break
         case "sair":
             console.log(" até a próxima ")
