d40823b3c435870827ea90fb421c0f1745db48ed
ItaloDEV21 <sampaioitalo21@gmail.com>
2024-09-12 17:16:50 -0300

    apresentar metas abertas

--- index.js
+++ index.js
@@ -29,14 +29,15 @@ const listarMeta = async () => {
         instructions: false
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length == 0){
         console.log("nenhuma meta selecionada!")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
+    
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
@@ -54,14 +55,30 @@ const metasRealizadas = async () => {
         return meta.checked
     })
 
-    if (realizadas.length == 0 ){
-        console.log('Não existem metas realizadas! :(')
+    if (realizadas.length < 1 ){
+        console.log("Não exisem metas realizadas! :(")
         return 
     }
 
     await select ({
-        message: "Metas realizadas ",
+        message: "Metas Realizadas ",
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        console.log('Não existem metas abertas! :)')
+        return 
+    }
+
+    await select ({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -85,6 +102,10 @@ const start = async () => {
                 value: "realizadas"
             },
             {
+                name: "Metas abertas",
+                value: "abertas"
+            },
+            {
                 name: "Sair", 
                 value: "sair"
             }
@@ -101,6 +122,9 @@ const start = async () => {
             break
         case "realizadas":
             await metasRealizadas()
+            break
+        case "abertas":
+            await metasAbertas()
             break
         case "sair":
             console.log(" até a próxima ")
